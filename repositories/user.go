9ff9088204f26dd806b72df6dbe676470217acb1
omar-sastec <omar.sastec@gmail.com>
2021-07-07 18:13:28 +0100

    fix repositories,services

--- repositories/user.go
+++ repositories/user.go
@@ -4,7 +4,7 @@ import (
 	"github.com/tekab-dev/tekab-test/models"
 )
 
-func ReturnUser() (user models.User) {
+func GetUser() (user models.User) {
 	User := models.User{"admin@gmail.com", "adminadmin"}
 	return User
 }
--- services/user.go
+++ services/user.go
@@ -2,8 +2,9 @@ package services
 
 import "github.com/tekab-dev/tekab-test/repositories"
 
-func ValidLogin(emial string, password string) (valid bool) {
-	if repositories.ReturnUser().Email == emial && repositories.ReturnUser().Password == password {
+func ValidLogin(email string, password string) (valid bool) {
+	user := repositories.GetUser()
+	if user.Email == email && user.Password == password {
 		return true
 	}
 	return false
