f0ee0b9557e6410a3f7f877d848b2b20b6e63e55
gloukil <ghassen.sastec@gmail.com>
2021-07-08 10:18:35 +0100

    request validation

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,27 +1,31 @@
 package controllers
 
 import (
+	"github.com/go-playground/validator"
+	_ "github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/tekab-dev/tekab-test/models"
 	"github.com/tekab-dev/tekab-test/services"
 )
 
 func Login(ctx *fiber.Ctx) error {
-	var body models.User
-	err := ctx.BodyParser(&body)
-	if err != nil {
+	var user models.User
+	validate := validator.New()
+	err := ctx.BodyParser(&user)
+	validationError := validate.Struct(user)
+	if err != nil || validationError != nil {
 		ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"error": "cannot parse json",
 		})
 		return nil
 	}
-	if !services.ValidLogin(body.Email, body.Password) {
+	if !services.ValidLogin(user.Email, user.Password) {
 		ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"error": "Bad Credentials",
 		})
 		return nil
 	}
-	token, err := services.CreateToken(body.Email)
+	token, err := services.CreateToken(user.Email)
 	if err != nil {
 		ctx.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
 			"error": err,
--- go.mod
+++ go.mod
@@ -5,8 +5,12 @@ go 1.16
 require (
 	github.com/andybalholm/brotli v1.0.3 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/gofiber/fiber/v2 v2.14.0
 	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/valyala/fasthttp v1.28.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
