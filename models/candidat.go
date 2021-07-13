9d7cf3b8ce398c1f98c168f97e239ec49bb866d4
gloukil <ghassen.sastec@gmail.com>
2021-07-13 14:25:32 +0100

    many to many database tables

--- models/candidat.go
+++ models/candidat.go
@@ -4,13 +4,14 @@ import "gorm.io/gorm"
 
 type Candidat struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" validate:"required,email" gorm:"unique"`
-	Note  []Note `json:"note"`
+	Name   string   `json:"name"`
+	Email  string   `json:"email" validate:"required,email" gorm:"unique"`
+	Note   []Note   `json:"note"`
+	Answer []Answer `json:"answer"`
 }
 type Note struct {
 	gorm.Model
-	CandidatID *uint64 `json:"candidat_id"`
-	UserID     *uint64 `json:"user_id" validate:"required"`
+	CandidatId *uint64 `json:"candidat_id"`
+	UserId     *uint64 `json:"user_id" validate:"required"`
 	Comment    string  `json:"comment" validate:"required"`
 }
---
+++ models/test_question.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type TestQuestion struct {
+	gorm.Model
+	QuestionId uint64   `json:"question_id" validate:"required"`
+	TestId     uint64   `json:"test_id" validate:"required"`
+	Answer     []Answer `json:"answer"`
+}
