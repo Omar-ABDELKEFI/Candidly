f3bfbf189f46106e0278a2e7bfa37b473a32a810
omar-sastec <omar.sastec@gmail.com>
2021-07-23 13:41:49 +0100

    Add Default cors config

--- server/server.go
+++ server/server.go
@@ -2,11 +2,13 @@ package server
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/tekab-dev/tekab-test/database"
 )
 
 func Init() {
 	app := fiber.New()
+	app.Use(cors.New())
 	database.GetDb()
 	Router(app)
 	database.MigrateDatabase()
