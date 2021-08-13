6ae12b0f57d0bc45a8a52632682fb3400eb56ad6
omar-sastec <omar.sastec@gmail.com>
2021-08-13 10:37:16 +0100

    remove ownModel from testQuestions model

--- models/test_question.go
+++ models/test_question.go
@@ -1,7 +1,11 @@
 package models
 
+import "time"
+
 type TestQuestion struct {
-	OwnModel
-	QuestionID uint64 `json:"question_id" validate:"required"`
-	TestID     uint64 `json:"test_id"`
+	ID         uint      `gorm:"primarykey"`
+	CreatedAt  time.Time `swaggerignore:"true"`
+	UpdatedAt  time.Time `swaggerignore:"true"`
+	QuestionID uint64    `json:"question_id" validate:"required"`
+	TestID     uint64    `json:"test_id"`
 }
