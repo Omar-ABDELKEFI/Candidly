d3a28598b08db94feaa6af97ded55d849f714358
omar-sastec <omar.sastec@gmail.com>
2021-09-26 13:34:28 +0100

    Select Candidate Emails

--- repositories/candidate.go
+++ repositories/candidate.go
@@ -1,21 +1,52 @@
 package repositories
 
 import (
+	"fmt"
 	"github.com/tekab-dev/tekab-test/database"
 	"github.com/tekab-dev/tekab-test/models"
 	"log"
 )
 
 func CreateCandidate(candidate []models.Candidate) ([]models.Candidate, error) {
-	log.Println("Creating Candidate ...")
+
 	db := database.DB
-	db.Model(&candidate).Association("test_candidate").Append()
+
+	fmt.Println("created candidate : ", candidate)
+	log.Println(candidate, "candidateCandidateCandidate\"")
 	err := db.Create(&candidate).Error
 
 	if err != nil {
 		return candidate, err
 	}
-	log.Println("created candidate : ", candidate)
-
+	for _, s := range candidate {
+		fmt.Printf("%+v\n", s.Email)
+		log.Println("oooo")
+	}
 	return candidate, nil
+}
+func SelectCandidateEmails() ([]string, error) {
+	log.Println("Creating Candidate ...")
+	var emails []string
+	db := database.DB
+	err := db.Table("candidates").Select("email").Scan(&emails).Error
+	log.Println(emails, "emailsemails")
+	if err != nil {
+		return emails, err
+	}
+
+	return emails, nil
+}
+func FindCandidate(emails []string) ([]models.Candidate, error) {
+	var candidatesId []models.Candidate
+	db := database.DB
+	err := db.Table("candidates").Where("email In ?", emails).Find(&candidatesId).Error
+	log.Println(emails, "aaaa")
+	if err != nil {
+		return candidatesId, err
+	}
+
+	for _, s := range candidatesId {
+		fmt.Printf("%+v\n candidatesId", s)
+	}
+	return candidatesId, nil
 }
